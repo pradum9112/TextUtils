e539c7fbd118df938348cd1afa86c6db22261f43
Pradum <pradumsonkar9112@gmail.com>
2023-05-07 21:30:14 +0530

    commit3

---
+++ module1.mjs
@@ -0,0 +1,8 @@
+// import ui from "./module2.mjs";
+import ui,{a,b,d} from "./module2.mjs";
+console.log(ui);
+console.log(d);
+console.log(b);
+console.log(a);
+
+
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a = "Pradum";
+const b = "Palak";
+const c = "Ram";
+const d = "Dhruv";
+
+export default c;
+export{a}
+export{b}   
+export{d}
